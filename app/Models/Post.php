30c557b4069171ba035caf617d17ec55c3aaf6fb
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-03 11:14:10 +0100

    function addded, return all posts staticly

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,18 +4,27 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
 class Post
 {
+    public static function all()
+    {
+
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(fn ($file) => $file -> getContents(), $files);
+
+    }
+
     //find file and fail safe if not found
-    public static function find($slug){
+    public static function find($slug)
+    {
 
-    if(! file_exists($path = resource_path("posts/{$slug}.html"))) {
-        throw new ModelNotFoundException();
+        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+            throw new ModelNotFoundException();
+        }
 
+        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
     }
-
-    return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
-    }
-   
 }
