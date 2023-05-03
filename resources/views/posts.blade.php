479452e7bddb7ca2b95ec73ee7be9057b2f93506
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-03 14:00:11 +0100

    Yaml Front Matte rinstalled and implemented

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,10 +9,19 @@
 <body>
 
     @foreach($posts as $post)
-        <article>
-            
-            {!!$post!!}
 
+        <article>
+            <h1><a href="/posts/{!!$post -> slug!!}">
+                {!!$post -> title!!}</h1>
+            </a>
+            <div>
+
+                {!!$post -> excerpt!!}
+
+            </div>
         </article>
+
+
+
        @endforeach
 </body>
