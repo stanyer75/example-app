7c0859f495b64a3daec091be7bca7cfcd0f88b62
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-03 08:42:30 +0100

    Laravel Install and a few bits done on laracasts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+    <body>
+        <article>
+            {!!$post!!}
+        </article>
+
+        <a href="/">Go Back</a>
+    </body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+    <body>
+        <article>
+        <h1><a href = "/posts/my-first-post">My First Post</a></h1>
+
+        <p>
+        Of course, you can change "example-app" in this URL to anything you like - just make sure the application name only contains alpha-numeric characters, dashes, and underscores. The Laravel application's directory will be created within the directory you execute the command from.
+
+        Sail installation may take several minutes while Sail's application containers are built on your local machine.
+
+        After the project has been created, you can navigate to the application directory and start Laravel Sail. Laravel Sail provides a simple command-line interface for interacting with Laravel's default Docker configuration:
+        </p>
+        </article>
+
+        <article>
+        <h1><a href = "/posts/my-second-post">My second Post</a></h1>
+
+        <p>
+        Of course, you can change "example-app" in this URL to anything you like - just make sure the application name only contains alpha-numeric characters, dashes, and underscores. The Laravel application's directory will be created within the directory you execute the command from.
+
+        Sail installation may take several minutes while Sail's application containers are built on your local machine.
+
+        After the project has been created, you can navigate to the application directory and start Laravel Sail. Laravel Sail provides a simple command-line interface for interacting with Laravel's default Docker configuration:
+        </p>
+        </article>
+
+        <article>
+        <h1><a href = "/posts/my-third-post">My Third Post</a></h1>
+
+        <p>
+        Of course, you can change "example-app" in this URL to anything you like - just make sure the application name only contains alpha-numeric characters, dashes, and underscores. The Laravel application's directory will be created within the directory you execute the command from.
+
+        Sail installation may take several minutes while Sail's application containers are built on your local machine.
+
+        After the project has been created, you can navigate to the application directory and start Laravel Sail. Laravel Sail provides a simple command-line interface for interacting with Laravel's default Docker configuration:
+        </p>
+        </article>
+    </body>
