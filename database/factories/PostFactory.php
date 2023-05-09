03f05af8cb0ccd373ba18dcd3fed2dee602f4713
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-09 10:36:20 +0100

    Category Factory added

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use app\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,7 +19,12 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'title' => $this->faker->sentence(),
+            'excerpt'=> $this->faker->sentence(),
+            'body' => $this->faker->paragraph(),
+
         ];
     }
 }
