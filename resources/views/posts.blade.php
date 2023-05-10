7c13d854d22e7a5a8a558f6370fc7ecd19855c0a
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-10 12:01:53 +0100

    feeding in data through the component and new

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,49 +4,17 @@
     @include ('_posts_header')
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        
-        
-        <x-post-featured-card type="data" :post="$posts[1]"/>
-        
+    
+        @if($posts->count())
 
-            <!-- <div class="lg:grid lg:grid-cols-2">
-                <x-post-card />
-                <x-post-card />
-            </div>
+            <x-posts-grid :posts="$posts"/>
 
-            <div class="lg:grid lg:grid-cols-3">
-                
-                <x-post-card />
-                <x-post-card />
-                <x-post-card />
+        @else
+         
+            <p class="text-center">No posts yet. Please check back later</p>
 
-            </div>    -->
-        </main> 
-    <!-- @foreach($posts as $post)
+        @endif
 
-    <article>
-        <h1>
-            <a href="/posts/{!!$post -> slug!!}">
-            {!!$post -> title!!}
-            </a>
-        </h1>
-
-            <p>
-            By <a href = "authors/{!!$post->author->username!!}">{!!$post->author->name!!}</a> in <a href="/categories/{!!$post->category->slug!!}">{!!$post->category->name!!}</a>
-            </p>
-
-            </p>
-
-
-        <div>
-
-            {!!$post -> excerpt!!}
-
-        </div>
-    </article>
-
-
-
-    @endforeach -->
+        </main>
 
 </x-layout>
