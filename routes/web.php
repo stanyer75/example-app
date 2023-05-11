0c23dfa2120b7ac7ec2e88ff6faca6785153bebc
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-11 14:14:27 +0100

    Category Dropdown component added

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -22,13 +21,6 @@ use Illuminate\Support\Facades\File;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('categories/{category:slug}',function(Category $category){
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
 
 Route::get('authors/{author:username}',function(User $author){
 
