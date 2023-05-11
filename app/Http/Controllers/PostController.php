d8745d1b2c6e9d3ead84bc879d554e7249014f6f
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-11 11:19:08 +0100

    Route through controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $posts = Post::latest();
+
+        if(request('search')) {
+
+            $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+
+        }
+
+        return view('posts', [
+            'posts' => $posts->get(),
+            'categories' => Category::all()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('post', [
+            'post' => $post,
+
+        ]);
+    }
 }
