d7e9654bfc88af01cbbc5f5e1d871b5f2bd119e8
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-11 13:13:24 +0100

    search feature added and configured

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,19 +11,8 @@ class PostController extends Controller
 {
     public function index()
     {
-
-        $posts = Post::latest();
-
-        if(request('search')) {
-
-            $posts
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-
-        }
-
         return view('posts', [
-            'posts' => $posts->get(),
+            'posts' => Post::latest()->filter(request(['search']))->get(),
             'categories' => Category::all()
         ]);
     }
