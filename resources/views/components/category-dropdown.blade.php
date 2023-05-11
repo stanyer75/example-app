ceaf3eadb66b8745a17b1299a4d7d53267bbdbef
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-11 14:58:04 +0100

    shift around and simplifying

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,3 +1,20 @@
 <div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+    <x-dropdown>
+        <x-slot name="trigger">
+            <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+
+                <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
+            </button>
+        </x-slot>
+
+        <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+
+        @foreach ($categories as $category)
+            <x-dropdown-item
+                href="/?category={{ $category->slug }}"
+                :active='request()->is("categories/{$category->slug}")'
+            >{{ ucwords($category->name) }}</x-dropdown-item>
+        @endforeach
+    </x-dropdown>
 </div>
