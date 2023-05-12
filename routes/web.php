f075464e62d2a862fb9012004ba576b199643767
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-12 09:52:08 +0100

    Register Controller addded and routed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,7 @@ use Illuminate\Support\Facades\File;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+Route::get('register', [RegisterController::class, 'create']);
 
 Route::get('authors/{author:username}',function(User $author){
 
